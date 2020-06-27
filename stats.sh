683cb154cb99a9444a3b48e1c13f843679367b0b
Raul <Raul.Valenzuela@alumnos.uv.cl>
2020-06-27 22:48:07 +0000

    problema2 terminado

--- stats.sh
+++ stats.sh
@@ -18,7 +18,7 @@ while getopts "d:h" opcion; do
 			forma_de_uso
 			;;
 		*)
-			echo "Para vizualizar la forma de uso utilice [-h]"
+			printf  "Para vizualizar la forma de uso utilice [-h]"
 			;;
 	esac
 done
@@ -59,8 +59,177 @@ do
 done
 printf "tsimTotal : promedio : min : max \n">>$tmpArch
 printf " memUsed: promedio : min : max \n" >> $tmpArch
-printf "%i : %i : %i : %i \n " $valorSum_pmm >> $tmpArch
+printf "%i : %.2f : %i : %i \n " $valorSum_pmm >> $tmpArch
 printf " %i : %.2f : %i: %i \n"  $valorMem_pmm >> $tmpArch
 rm -f $tmp1 $tmp2
+
+#problema 2---------------------------------------------------------------------------------------------------------
+
+summary=(`find $dataIn -name '*.txt' -print | sort | grep summary | grep -v '._'`)
+
+tmpEva="evacuation.txt"
+tmp3="temporal.txt"
+tmp4="temporal2.txt"
+tmp5="temporal3.txt"
+tmp6="temporal4.txt"
+tmp7="temporal5.txt"
+tmp8="temporal6.txt"
+tmp9="temporal7.txt"
+tmp10="temporal8.txt"
+tmp11="temporal9.txt"
+tmp12="temporal10.txt"
+tmp13="temporal11.txt"
+rm -f $tmpEva
+rm -f $tmp3
+rm -f $tmp4
+rm -f $tmp5
+rm -f $tmp6
+rm -f $tmp7
+rm -f $tmp8
+rm -f $tmp9
+rm -f $tmp10
+rm -f $tmp11
+rm -f $tmp12
+rm -f $tmp13
+
+for i in ${summary[*]};
+do
+        personasT=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{pSum=0}{pSum+=$8}END{print pSum}' )
+        printf "$personasT \n" >> $tmp3
+        personas_pmm=$(cat $tmp3 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp3<min){min=$tmp3};\
+                                                                                                  if($tmp3>max){max=$tmp3};\
+                                                                                                  total+=$tmp3; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+
+	residentesT=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{rSum=0}{if($3==0)rSum+=$8}END{print rSum}' )
+        printf "$residentesT \n" >> $tmp4
+        residentes_pmm=$(cat $tmp4 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp4<min){min=$tmp4};\
+                                                                                                  if($tmp4>max){max=$tmp4};\
+                                                                                                  total+=$tmp4; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+
+        visitantesT=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{vSum=0}{if($3==1)vSum+=$8}END{print vSum}' )
+        printf "$visitantesT \n" >> $tmp5
+        visitantes_pmm=$(cat $tmp5 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp5<min){min=$tmp5};\
+                                                                                                  if($tmp5>max){max=$tmp5};\
+                                                                                                  total+=$tmp5; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+
+	residentesG0=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{rG0Sum=0}{if($3==0 && $4==0) rG0Sum+=$8}END{print rG0Sum}' )
+        printf "$residentesG0 \n" >> $tmp6
+        residentesG0_pmm=$(cat $tmp6 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp6<min){min=$tmp6};\
+                                                                                                  if($tmp6>max){max=$tmp6};\
+                                                                                                  total+=$tmp6; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+
+
+
+	residentesG1=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{rG1Sum=0}{if($3==0 && $4==1) rG1Sum+=$8}END{print rG1Sum}' )
+        printf "$residentesG1 \n" >> $tmp7
+        residentesG1_pmm=$(cat $tmp7 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp7<min){min=$tmp7};\
+                                                                                                  if($tmp7>max){max=$tmp7};\
+                                                                                                  total+=$tmp7; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+	residentesG2=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{rG2Sum=0}{if($3==0 && $4==2) rG2Sum+=$8}END{print rG2Sum}' )
+        printf "$residentesG2 \n" >> $tmp8
+        residentesG2_pmm=$(cat $tmp8 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp8<min){min=$tmp8};\
+                                                                                                  if($tmp8>max){max=$tmp8};\
+                                                                                                  total+=$tmp8; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+
+	residentesG3=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{rG3Sum=0}{if($3==0 && $4==3) rG3Sum+=$8}END{print rG3Sum}' )
+        printf "$residentesG3 \n" >> $tmp9
+        residentesG3_pmm=$(cat $tmp9 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp9<min){min=$tmp9};\
+                                                                                                  if($tmp9>max){max=$tmp9};\
+                                                                                                  total+=$tmp9; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+
+	visitantesG0=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{vG0Sum=0}{if($3==1 && $4==0) vG0Sum+=$8}END{print vG0Sum}' )
+        printf "$visitantesG0 \n" >> $tmp10
+        visitantesG0_pmm=$(cat $tmp10 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp10<min){min=$tmp10};\
+                                                                                                  if($tmp10>max){max=$tmp10};\
+                                                                                                  total+=$tmp10; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+
+
+	visitantesG1=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{vG1Sum=0}{if($3==1 && $4==1) vG1Sum+=$8}END{print vG1Sum}' )
+        printf "$visitantesG1 \n" >> $tmp11
+        visitantesG1_pmm=$(cat $tmp11 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp11<min){min=$tmp11};\
+                                                                                                  if($tmp11>max){max=$tmp11};\
+                                                                                                  total+=$tmp11; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+	visitantesG2=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{vG2Sum=0}{if($3==1 && $4==2) vG2Sum+=$8}END{print vG2Sum}' )
+        printf "$visitantesG2 \n" >> $tmp12
+        visitantesG2_pmm=$(cat $tmp12 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp12<min){min=$tmp12};\
+                                                                                                  if($tmp12>max){max=$tmp12};\
+                                                                                                  total+=$tmp12; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+	visitantesG3=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{vG3Sum=0}{if($3==1 && $4==3) vG3Sum+=$8}END{print vG3Sum}' )
+        printf "$visitantesG3 \n" >> $tmp13
+        visitantesG3_pmm=$(cat $tmp13 | awk 'BEGIN{ min=2**63-1; max=0}{ if($tmp13<min){min=$tmp13};\
+                                                                                                  if($tmp13>max){max=$tmp13};\
+                                                                                                  total+=$tmp13; count+=1;\
+                                                                                            } \
+                                                                                                 END { print total, total/count, min, max}')
+done
+printf "alls : promedio : min : max \n" >> $tmpEva
+printf "residents : promedio : min : max \n" >>$tmpEva
+printf "visitorsI : promedio : min : max \n" >>$tmpEva
+printf "residents-G0 : promedio : min : max \n" >> $tmpEva
+printf "residents-G1 : promedio : min : max \n" >> $tmpEva
+printf "residents-G2 : promedio : min : max \n" >> $tmpEva
+printf "residents-G3 : promedio : min : max \n" >> $tmpEva
+printf "visitorsI-G0 : promedio : min : max \n" >> $tmpEva
+printf "visitorsI-G1 : promedio : min : max \n" >> $tmpEva
+printf "visitorsI-G2 : promedio : min : max \n" >> $tmpEva
+printf "visitorsI-G3 : promedio : min : max \n" >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $personas_pmm  >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $residentes_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $visitantes_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $residentesG0_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $residentesG1_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $residentesG2_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $residentesG3_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $visitantesG0_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $visitantesG1_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $visitantesG2_pmm >> $tmpEva
+printf "%i : %.2f : %i : %i \n" $visitantesG3_pmm >> $tmpEva
+
+rm -f $tmp3
+rm -f $tmp4
+rm -f $tmp5
+rm -f $tmp6
+rm -f $tmp7
+rm -f $tmp8
+rm -f $tmp9
+rm -f $tmp10
+rm -f $tmp11
+rm -f $tmp12
+rm -f $tmp13
+
+#Problema 3--------------------------------------------------------------------------------------------------------------------------
+#tmpPhone="usePhone-stats.txt"
+
+#rm -f $tmpFile
+#for i in ${usePhoneFiles[*]};
+#do
+       # printf "> %s\n" $i
+      #  tiempos=(`cat $i | tail -n+3 | cut -d ':' -f 3`)
+     #   for i in ${tiempos[*]};
+    #    do
+   #             printf "%d:" $i >> $tmpFile
+  #      done
+ #       printf "\n" >> $tmpFile
+#done
 
 
